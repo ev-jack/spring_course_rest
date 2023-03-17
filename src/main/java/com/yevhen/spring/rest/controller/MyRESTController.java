e1f62ec4561fc40546c71fe008934f31974a8f31
ev-jack <jolyjeck@gmail.com>
2023-03-18 00:39:42 +0200

    my first commit

---
+++ src/main/java/com/yevhen/spring/rest/controller/MyRESTController.java
@@ -0,0 +1,24 @@
+package com.yevhen.spring.rest.controller;
+
+import com.yevhen.spring.rest.entity.Employee;
+import com.yevhen.spring.rest.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class MyRESTController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/employees")
+    public List<Employee> showAllEmployees() {
+        List<Employee> allEmployees = employeeService.getAllEmployees();
+        return allEmployees;
+    }
+}
